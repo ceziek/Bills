ca10742dc96c6e203512e6d05f3518a5a1c38660
piotr.pliszczynski <piotr.pliszczynski@cgi.com>
2016-03-29 10:07:34 +0200

    few comments

--- app/services/bills_service.js
+++ app/services/bills_service.js
@@ -11,12 +11,36 @@ angular.module('myApp.BillsService', [])
         };
 
         this.add = function (item) {
-            item.id = Math.round(new Date() / 1000);
+            // lepiej uzyc
+            item.id = (new Date()).getTime();
+
+            // item.id = Math.round(new Date() / 1000);
+
             item.items = [];
             $localStorage.bills.push(item);
         };
 
         this.delete = function (object) {
+            // to jest dosc niebezpieczne
+            // prosty przyklad
+            /*
+
+            let arr = ['a','b','c'];
+
+            chcemy id elementu 'b' ... wiec indexOf('b') da nam 1
+
+            ale
+
+            let arr = ['a','a','a','b','c','c','a'];
+
+            indexOf('a') === 0
+            indexOf('c') === 4
+
+            czyli zawsze id pierwszego elementu
+
+             */
+
+
             $localStorage.bills.splice($localStorage.bills.indexOf(object), 1);
         };
 
